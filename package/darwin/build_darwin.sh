67437877e03adf0cd4cdb1209595f9ff6d31c23f
Humphrey Shotton <humpheh@gmail.com>
2018-11-16 20:01:01 +0000

    Build darwin binary bundles

---
+++ package/darwin/build_darwin.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+OUTPUT=../build/Goboy.app
+
+# Remove any existing darwin app
+if [[ -f ${OUTPUT} ]]; then
+    rm -r ${OUTPUT}
+fi
+
+# Make the app and the contents directory
+mkdir -p ${OUTPUT}/Contents
+
+# Copy the contents into the app contents folder
+cp -Rp Contents/ ${OUTPUT}/Contents
+
+# Build GoBoy or copy if argument is passed in
+if [[ $1 == "" ]]; then
+    # Build the binary into the contents executable
+    go build -o ${OUTPUT}/Contents/MacOS/GoBoy github.com/Humpheh/goboy/cmd/goboy
+else
+    # Move the input file to the contents executable
+    cp $1 ${OUTPUT}/Contents/MacOS/GoBoy
+fi
---
+++ package/windows/build_windows.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+OUTPUT=../build/Goboy.app
+
+cp $1 ${OUTPUT}/GoBoy.exe
