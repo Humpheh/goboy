b6a2a4590d602e6219ba18da1768968cf7f9a234
Humphrey Shotton <humpheh@gmail.com>
2018-11-23 08:28:11 +0000

    Test building darwin again

--- package/darwin/build_darwin.sh
+++ package/darwin/build_darwin.sh
@@ -10,10 +10,10 @@ if [[ -f ${OUTPUT} ]]; then
 fi
 
 # Make the app and the contents directory
-mkdir -p ${OUTPUT}/Contents
+mkdir -p ${OUTPUT}
 
 # Copy the contents into the app contents folder
-cp -Rp Contents/ ${OUTPUT}/Contents
+cp -R Contents/ ${OUTPUT}/Contents
 #sed 's/VERSION/'${MYV}'/g' package/darwin/Contents/Info.plist TODO: sort version
 
 # Build GoBoy or copy if argument is passed in
@@ -21,6 +21,8 @@ if [[ $1 == "" ]]; then
     # Build the binary into the contents executable
     go build -o ${OUTPUT}/Contents/MacOS/GoBoy github.com/Humpheh/goboy/cmd/goboy
 else
+    # Ensure the folder exists
+    mkdir -p ${OUTPUT}/Contents/MacOS/GoBoy
     # Move the input file to the contents executable
     cp $1 ${OUTPUT}/Contents/MacOS/GoBoy
 fi
