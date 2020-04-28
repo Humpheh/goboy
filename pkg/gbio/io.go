c28f45a8cacbd689660b011b5363ba2984ce9a62
Luuk Verweij <luuk_verweij@msn.com>
2020-04-28 16:10:39 +0200

    remove unnecessary methods from IOBinding interface

--- pkg/gbio/io.go
+++ pkg/gbio/io.go
@@ -2,12 +2,8 @@ package gbio
 
 // IOBinding provides an interface for display and input bindings.
 type IOBinding interface {
-	// Init the IOBinding
-	Init(disableVsync bool)
 	// RenderScreen renders a frame of the game.
 	RenderScreen()
-	// Destroy the IOBinding instance.
-	Destroy()
 	// ProcessInput processes input.
 	ProcessInput()
 	// SetTitle sets the title of the window.
